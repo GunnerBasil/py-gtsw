86c5e11122a9383a39029722c704af93d122cda8
GunnerBasil <57513430+GunnerBasil@users.noreply.github.com>
2020-01-03 15:43:32 -0700

    v2.1

--- warp.py
+++ warp.py
@@ -1,5 +1,7 @@
 from googletrans import Translator
 
+# v2.0
+GTSW_version = 2.0
 tr = Translator()
 def warp_once(text): # this is the script to run text through google translate once
     stg1 = tr.translate(text, dest="es",src="en")
@@ -9,17 +11,30 @@ def warp_once(text): # this is the script to run text through google translate o
     stg5 = tr.translate(stg4.text, dest="en",src="hr")
     return stg5.text
         
-def warp(text,times): # this script is used to warp the text multiple times
+def warpstat(text,times): # this script is used to warp the text multiple times with status updates
     for index in range(times):
         if (index==0):
             L = warp_once(text)
+            print('Warped 1 time' )
         else:
             L = warp_once(L)
+            print('Warped '+ str(int(index)+1)+' times' )
+    return L
+    
+def warp(text,times): # this script is used to warp the text multiple times without status updates
+    for index in range(times):
+        if (index==0):
+            L = warp_once(text)
+            print('Warped 1 time' )
+        else:
+            L = warp_once(L)
+            print('Warped '+ str(int(index)+1)+' times' )
     return L
 
-use_example = True # change to false if you want to use this as a module
+use_example = True # change to False if you want to use this as a module
 if (use_example == True):
+    print('Google Translate String Warp v'+str(GTSW_version))
     text = input('Text to warp: ')
     times = int(input('The power of the warp: '))
-    warptext = warp(text,times)
+    warptext = warpstat(text,times)
     print(warptext)
